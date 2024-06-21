f91e7a34fa75a2bb1254a39984ded41c22af6415
unknown <abhisheksindhwal11@gmail.com>
2024-06-21 16:05:27 +0530

    error fixes

--- src/app/creator/creator.component.ts
+++ src/app/creator/creator.component.ts
@@ -48,27 +48,23 @@ export class CreatorComponent  implements OnInit {
     this.contactForm = this.fb.group({
       category: [null],
       title:[null],
-      skills:this.fb.array([
-        
-      ]),
-      addOns: this.fb.array([
-        new FormControl(null,),
-      ]),
-      additionalInfo:this.fb.array([
-        new FormControl(null,),
-      ]),
+      skill:this.fb.array([]),
+      addOns: this.fb.array([]),
+      additionalInfo:this.fb.array([]),
       text: new FormControl(),
       level:['',Validators.required],
       paymentType:['0',Validators.required],
       amount:[''],
       min:[],
       max:[]
-
     });
   }
 
 
   ngOnInit() {
+
+    console.log("this is skillst", this.getSkills);
+    
   }
 
 
@@ -76,13 +72,6 @@ export class CreatorComponent  implements OnInit {
   //Code for payment type form control
   get paymentType():string { return this.contactForm.get('paymentType')?.value; }
 
-
-
-  //Code for ADD Ons (Toggle)
-  hideSingleSelectionIndicator = signal(false);
-  toggleSingleSelectionIndicator() {
-    this.hideSingleSelectionIndicator.update(value => !value);
-  }
 
 
   // Dummy Data
@@ -95,40 +84,71 @@ export class CreatorComponent  implements OnInit {
   ];
 
   addOns=[
-  {addOn:"This is First Add On"},
-    {addOn:"This is Second Add On"},
-    {addOn:"This is third Add on"}
+    {id:0, addOn:"This is First Add On", checked : false},
+    {id:1, addOn:"This is Second Add On", checked : false},
+    {id:2, addOn:"This is third Add on", checked : false}
   ]
+
+
+  //Code for ADD Ons (Toggle)
+  hideSingleSelectionIndicator = signal(false);
+  toggleSingleSelectionIndicator() {
+    this.hideSingleSelectionIndicator.update(value => !value);
+  }
+  addOnChanged(id:number, event: any)
+  {
+    const checkbox = this.addOns.find(cb => cb.id===id)
+    if(checkbox)
+    {
+      checkbox.checked = event.checked;
+      checkbox.checked ? this.addAddOn(checkbox) : this.removeAddOn(checkbox)
+    } 
+  }
+  get addOn() : FormArray{
+    return this.contactForm.get("addOns") as FormArray
+  }
+  addAddOn(checkbox : any)
+  {
+    this.addOn.push(new FormControl(checkbox)) 
+  }
+  removeAddOn(checkbox:any)
+  {
+    const index = this.addOn.controls.findIndex(item => item.value===checkbox);
+    this.addOn.removeAt(index);
+  }
+
   
-
-
-
   //Code for chip Angular Material
+
   readonly addOnBlur = true;
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
   readonly skills = signal<Skill[]>([]);
   readonly announcer = inject(LiveAnnouncer);
+  get getSkills():FormArray{
+    return this.contactForm.get("skill") as FormArray
+  }
 
   add(event: MatChipInputEvent): void {
     const value = (event.value || '').trim();
-
     // Add our skill
+    // console.log("this is value");
+    
     if (value) {
       this.skills.update(skills => [...skills, {name: value}]);
+      this.getSkills.push(new FormControl(value))
     }
-
     // Clear the input value
     event.chipInput!.clear();
-  }
 
+  }
   remove(skill: Skill): void {
     this.skills.update(skills => {
       const index = skills.indexOf(skill);
       if (index < 0) {
         return skills;
       }
-
       skills.splice(index, 1);
+      this.getSkills.removeAt(index);
       this.announcer.announce(`Removed ${skill.name}`);
       return [...skills];
     });
@@ -159,14 +179,15 @@ export class CreatorComponent  implements OnInit {
     return this.contactForm.get("additionalInfo") as FormArray
   }
   addInfo(){
-    this.additionalInfo.push(new FormControl(null))
+    const item=this.fb.group({
+      additionalInfo:['',Validators.required]
+    })
+    this.additionalInfo.push(item)
   }
   removeInfo(i:number){
     this.additionalInfo.removeAt(i);
   }
-
-
-
+  
 
   //Code for form submission
   submit() {
